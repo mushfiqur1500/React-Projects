3fa6afe9ebc4b9a3a610fd0800b4a4c5abf251c6
Mohammad Mushfiqur Rahman <mushfiqur1500@gmail.com>
2024-06-11 00:36:43 +0600

    AppNAme Update

--- Projects/Todo-App/src/App.jsx
+++ Projects/Todo-App/src/App.jsx
@@ -1,14 +1,42 @@
+import AppName from "./components/AppName";
+
 function App() {
   return (
     <center className="todo-container">
-      <h1>Todo App</h1>
+      <AppName />
       <div className="container text-center">
         <div className="row">
           <div className="col-6">
             <input type="text" placeholder="Enter Todo Here" />
           </div>
-          <div className="col-4">col-8</div>
-          <div className="col-2">col-4</div>
+          <div className="col-4">
+            <input type="Date" />
+          </div>
+          <div className="col-2">
+            <button type="button" className="btn btn-success">
+              Add
+            </button>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-6">Buy Milk</div>
+          <div className="col-4">11/06/2024</div>
+          <div className="col-2">
+            <button type="button" className="btn btn-danger">
+              Delete
+            </button>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-6">Go to College</div>
+          <div className="col-4">11/06/2024</div>
+          <div className="col-2">
+            <button type="button" className="btn btn-danger">
+              Delete
+            </button>
+          </div>
         </div>
       </div>
     </center>
