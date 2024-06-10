269fb1f8e4fd6b443940516296ca8fb9ed345d99
Mohammad Mushfiqur Rahman <mushfiqur1500@gmail.com>
2024-06-10 22:44:27 +0600

    added

---
+++ Projects/Todo-App/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  
+
+  return (
+    <center className="todo-container">
+      <h1>Todo App</h1>
+      <div class="container text-center">
+          <div class="row">
+            <div class="col-8">col-8</div>
+            <div class="col-4">col-4</div>
+            </div>
+          </div>
+    </center>
+  );
+}
+
+export default App
