e23a301dfcf3a34f54fee59ace11f2214013920a
Mohammad Mushfiqur Rahman <mushfiqur1500@gmail.com>
2024-06-11 00:06:57 +0600

    Todo add

---
+++ Projects/Todo-App/src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <center className="todo-container">
+      <h1>Todo App</h1>
+      <div className="container text-center">
+        <div className="row">
+          <div className="col-6">
+            <input type="text" placeholder="Enter Todo Here" />
+          </div>
+          <div className="col-4">col-8</div>
+          <div className="col-2">col-4</div>
+        </div>
+      </div>
+    </center>
+  );
+}
+
+export default App
