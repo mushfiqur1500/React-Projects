aff8e52ea24ff78dbf079240d9fb120c50f7105e
Mohammad Mushfiqur Rahman <mushfiqur1500@gmail.com>
2024-06-16 16:05:17 +0600

    Calculator

--- Projects/calculator-virsion-1/calculator/src/App.jsx
+++ Projects/calculator-virsion-1/calculator/src/App.jsx
@@ -1,11 +1,19 @@
-import './App.css'
+import styles from "./App.module.css";
 
 function App() {
 
   return (
-    <>
-    </>
-  )
+    <div className={styles.calculator}>
+      <input className={styles.display} type="text" />
+      <div className={styles.buttonscontainer}>
+        <div className={styles.button}></div>
+        <button className={styles.button}>C</button>
+        <button className={styles.button}>1</button>
+        <button className={styles.button}>2</button>
+        <button className={styles.button}>+</button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
