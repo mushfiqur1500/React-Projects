7222fa49d91c9cc685a4cdc86074b67f0182e0c4
Mohammad Mushfiqur Rahman <mushfiqur1500@gmail.com>
2024-06-16 15:25:33 +0600

    small change Todo App

--- Projects/Todo-App/src/components/TodoItem1.jsx
+++ Projects/Todo-App/src/components/TodoItem1.jsx
@@ -1,20 +1,19 @@
-function Todoitem1 (){
-let TodoName = "Got to College";
-let todoDate = "11/06/2024";
-return (
-  <div className="container ">
-    <div className="row kg-row">
-      <div className="col-6">{TodoName}</div>
-      <div className="col-4">{todoDate}</div>
-      <div className="col-2">
-        <button type="button" className="btn btn-danger button">
-          Delete
-        </button>
+function Todoitem1() {
+  let TodoName = "Got to College";
+  let todoDate = "11/06/2024";
+  return (
+    <div className="container ">
+      <div className="row kg-row">
+        <div className="col-6">{TodoName}</div>
+        <div className="col-4">{todoDate}</div>
+        <div className="col-2">
+          <button type="button" className="btn btn-danger button">
+            Delete
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-);
-
+  );
 }
 
 export default Todoitem1;
