39d43ce1f12a4481d96fe098fd5afa0e657fe2f4
Mohammad Mushfiqur Rahman <mushfiqur1500@gmail.com>
2024-06-22 00:10:26 +0600

    All button Added

--- Projects/calculator-virsion-1/calculator/src/components/ButtonsContainer.jsx
+++ Projects/calculator-virsion-1/calculator/src/components/ButtonsContainer.jsx
@@ -1,16 +1,35 @@
-
-import styles from "./ButtonsContainer.module.css"
+import styles from "./ButtonsContainer.module.css";
 
 const ButtonsContainer = () => {
-    return(
-      <div className={styles.buttonscontainer}>
-        <div className={styles.button}></div>
-        <button className={styles.button}>C</button>
-        <button className={styles.button}>1</button>
-        <button className={styles.button}>2</button>
-        <button className={styles.button}>+</button>
-      </div>
-      );
+  const buttonNames = [
+    "C",
+    "1",
+    "2",
+    "+",
+    "3",
+    "4",
+    "-",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "/",
+    "=",
+    "9",
+    "0",
+    ".",
+  ];
+
+  return (
+    <div className={styles.buttonsContainer}>
+      {buttonNames.map((buttonName) => (
+        <button key={buttonName} className={styles.button}>
+          {buttonName}
+        </button>
+      ))}
+    </div>
+  );
 };
 
-export default ButtonsContainer
+export default ButtonsContainer;
